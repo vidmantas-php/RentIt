4a9b0c08a66414f4c0ed35b59cb8ce2774a88be8
vidma65 <vidma65@gmail.com>
2020-11-09 20:28:28 +0200

    categories

--- src/components/Cards/CategoryCard/CategoryCard.js
+++ src/components/Cards/CategoryCard/CategoryCard.js
@@ -7,7 +7,7 @@ export const CategoryCard = ({ categoryImage, categoryName, redirect }) => {
       <div>
         <img src={categoryImage} className="card-img-top border__radius__50proc" alt={categoryName} />
         <div className="text-center">
-          <h5 className="text__color__black">{categoryName}</h5>
+          <h5 className="text__color__black text__font">{categoryName}</h5>
         </div>
       </div>
     </a>
